8684553f7782a4d9ddcdf181388b8fce38d671f9
bala-amypo <bala@amypo.in>
2025-12-18 05:48:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    StudentService ser;
+    
+    @PostMapping("/add_data")
+
+    public Student createData(@RequestBody Student stu){
+        return ser.createData(stu);
+    }
+
+    @GetMapping("/fetch_data")
+    public List<Student> fetchRecord(){
+        return ser.fetchRecord();
+    }
+
+    @GetMapping("/fetch_data_id/{id}")
+    public Optional<Student> fetchDataById(@PathVariable int id){
+        return ser.fetchDataById(id);
+    }
+
+    @PutMapping("update_data/{id}")
+    public String updateDataById(@PathVariable int id, @RequestBody Student stu) {
+        stu.setId(id);
+        ser.createData(stu);
+        return "Data updated successfully";
+    }
+}
---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.entity.Student;
+
+public interface StudentService {
+    Student createData(Student stu);
+    List<Student> fetchRecord();
+    Optional<Student> fetchDataById(int id);
+}
